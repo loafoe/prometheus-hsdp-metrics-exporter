5b8947013eb316816da269833307794a035d1e33
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-09-19 12:22:14 +0200

    RabbitMQ: add exchanges total and queued messages total

--- main.go
+++ main.go
@@ -138,6 +138,21 @@ func main() {
 		hsdp.WithHelp("The total number of objects in the bucket"),
 		hsdp.WithQuery("(s3_objects)  * on (hsdp_instance_guid) group_left(hsdp_instance_name)(cf_service_instance_info{hsdp_instance_name=~\".*\"})"))
 
+	// RabbitMQ
+	rabbitmqQeuedMessagesTotalMetric, _ := hsdp.NewMetric(
+		hsdp.WithClient(uaaClient),
+		hsdp.WithService("rabbitmq"),
+		hsdp.WithName("rabbitmq_queued_messages_total"),
+		hsdp.WithHelp("The total number of queued messages"),
+		hsdp.WithQuery("(rabbitmq_queue_messages_total) * on (hsdp_instance_guid) group_left(hsdp_instance_name)(cf_service_instance_info{hsdp_instance_name=~\".*\"})"),
+	)
+	rabbitmqExchangesTotalMetric, _ := hsdp.NewMetric(
+		hsdp.WithClient(uaaClient),
+		hsdp.WithService("rabbitmq"),
+		hsdp.WithName("rabbitmq_exchanges_total"),
+		hsdp.WithHelp("Total number of exchanges"),
+		hsdp.WithQuery("(rabbitmq_exchangesTotal) * on (hsdp_instance_guid) group_left(hsdp_instance_name)(cf_service_instance_info{hsdp_instance_name=~\".*\"})"))
+
 	registry.MustRegister(rdsCPMetric)
 	registry.MustRegister(rdsDatabaseConnectionsMetric)
 	registry.MustRegister(rdsFreeStorageMetric)
@@ -146,6 +161,8 @@ func main() {
 	registry.MustRegister(rdsWriteOPSMetrics)
 	registry.MustRegister(s3BucketSizeMetrics)
 	registry.MustRegister(s3ObjectsMetrics)
+	registry.MustRegister(rabbitmqQeuedMessagesTotalMetric)
+	registry.MustRegister(rabbitmqExchangesTotalMetric)
 
 	go func() {
 		sleep := false
@@ -174,6 +191,8 @@ func main() {
 				_ = rdsWriteOPSMetrics.Update(ctx, instance)
 				_ = s3BucketSizeMetrics.Update(ctx, instance)
 				_ = s3ObjectsMetrics.Update(ctx, instance)
+				_ = rabbitmqQeuedMessagesTotalMetric.Update(ctx, instance)
+				_ = rabbitmqExchangesTotalMetric.Update(ctx, instance)
 			}
 		}
 	}()
